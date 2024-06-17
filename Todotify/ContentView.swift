e33128c26a7410adccff98794da2a11dad1a9f05
kalmahik <kalmahik@yahoo.com>
2024-06-17 07:30:06 +0300

    Initial Commit

---
+++ Todotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todotify
+//
+//  Created by kalmahik on 17.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todotify/TodotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodotifyApp.swift
+//  Todotify
+//
+//  Created by kalmahik on 17.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
